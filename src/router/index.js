8dbb6217df3655971cbfaa261f24fb360232d791
devjmv <javier161890@gmail.com>
2024-08-14 22:55:33 +0200

    add listanimalsview

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import ListAnimalsView from '@/views/ListAnimalsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: ListAnimalsView
     }
   ]
 })
